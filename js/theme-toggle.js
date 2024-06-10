b2400955b62c8711008bd5d60b587ec77b426241
Doug Hill <stix2you@gmail.com>
2024-06-10 11:18:46 -0500

    Made new logo responsive with navbar items, fixed theme-toggle undefined after removing logo.png

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -6,25 +6,16 @@ function toggleTheme() {   // function to toggle the theme
    let isActive = document.getElementById('toggle').classList.toggle("active");  // toggle the active class on the toggle button
    localStorage.setItem('theme', isDark ? 'dark' : 'light');   // save the theme preference in local storage
    localStorage.setItem('active', isActive ? 'active' : 'inactive');   // save the active preference in local storage   
-   let logo = document.querySelector('.logo_image');  // get the logo image element
-    if (isDark) {
-        logo.src = 'img/logo_inverse.png';  // set to inverse logo for dark theme
-    } else {
-        logo.src = 'img/logo.png';  // set back to normal logo for light theme
-    }
 }
 
 function loadTheme() {
    const savedTheme = localStorage.getItem('theme');  // get the saved theme preference from local storage
-   let logo = document.querySelector('.logo_image');  // get the logo image element
    if (savedTheme === 'dark') {
        document.body.classList.add('dark-theme');
        document.getElementById('toggle').classList.remove('active');
-       logo.src = 'img/logo_inverse.png';  // set to inverse logo for dark theme
    } else {
        document.body.classList.remove('dark-theme');
        document.getElementById('toggle').classList.add('active');
-       logo.src = 'img/logo.png';  // set back to normal logo for light theme
    }
 }
 
