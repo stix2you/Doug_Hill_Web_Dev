13527205d58848b6ef1e059e19f1d1874c38ac3c
Doug Hill <stix2you@gmail.com>
2024-06-10 10:49:47 -0500

    restyled logo

--- js/color-change.js
+++ js/color-change.js
@@ -15,32 +15,28 @@ const colorVariables = [
 //    return "#" + randomHex.toString(16).padStart(6, '0');
 // }
 
-// generate a random color with a minimum difference between RGB components and a minimum brightness
+// generate a random color with an upper brightness threshold
 function getRandomColor() {
    let r, g, b;
-   let brightnessThreshold = 50;  // Lower limit for brightness to avoid very dark colors
-   let differenceThreshold = 50;  // Minimum difference required to avoid grayish colors
+   let maxBrightness = 50;  // Upper limit for brightness to keep colors on the darker end
 
    do {
        r = Math.floor(Math.random() * 256);  // Random red
        g = Math.floor(Math.random() * 256);  // Random green
        b = Math.floor(Math.random() * 256);  // Random blue
-   } while ((Math.max(r, g, b) < brightnessThreshold) ||  // Ensure it's not too dark
-            (Math.abs(r - g) < differenceThreshold &&     // Avoid grayish by ensuring there's enough difference between the RGB components
-             Math.abs(g - b) < differenceThreshold &&     
-             Math.abs(b - r) < differenceThreshold));
+   } while ((r + g + b) / 3 > maxBrightness);  // Ensure average brightness is below the threshold
 
    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1); // Convert to hex and attach the # symbol, return 
 }
 
-// Usage: Assign a random color value
+// Assign a random color value
 const randomColor = getRandomColor();   // Call the function to get a random color
-
 
 let currentIndex = 0;  // Initialize the index of the color variable
 
 function updateColor() {
    const randomColor = getRandomColor();   // call function to get a random color
+   console.log(randomColor);  // Log the random color to the console
    document.documentElement.style.setProperty(colorVariables[currentIndex], randomColor);  // Set the color to the root element
    currentIndex = (currentIndex + 1) % colorVariables.length;                             // Move to the next color variable in the root element
    setTimeout(updateColor, 30000);                                                     // Call the function again after 2 seconds   
